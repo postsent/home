808f07bbba18d6b7d7e5adc63450a3c9355bab1e
Jerry <60166597+postsent@users.noreply.github.com>
2024-08-12 20:18:27 +0800

    fix toc not collapse by update toc content

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 # run below with wsl2
+# or run by: ./run.sh
 
 jupyter-book build .
 cp -TRv _build/html docs/ > /dev/null # overwrite https://stackoverflow.com/questions/23698183/how-to-force-cp-to-overwrite-directory-instead-of-creating-another-one-inside
